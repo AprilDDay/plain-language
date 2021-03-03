0b594d9399b5d029f4188453f0e4cdbca8d699da
AprilDDay <61091062+AprilDDay@users.noreply.github.com>
2021-03-03 09:56:57 +0000

    Add files via upload

---
+++ plain-language/src/ie/gmit/dip/Files.java
@@ -0,0 +1,19 @@
+package ie.gmit.dip;
+
+/**
+ * This is an interface. Any class that declares that it is implementing 
+ * this interface has these values.
+ */
+public interface Files {
+
+	/**
+	 * File for Google Words.
+	 */
+	static final String googleWordFile = "./google-1000.txt";
+	
+	/**
+	 * File for Moby Thesaurus words.
+	 */
+	static final String mobyThesaurus2File = "./MobyThesaurus2.txt";
+
+}
